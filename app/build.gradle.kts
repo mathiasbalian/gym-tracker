d37713d53bd9ca42296ee31066da016a94411908
mathiasbalian <mathias.balian92@gmail.com>
2023-11-24 18:30:52 +0100

    added dependencies in comments

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    // id("com.google.devtools.ksp")
 }
 
 android {
@@ -51,6 +52,11 @@ dependencies {
     implementation("androidx.navigation:navigation-ui-ktx:2.7.5")
     implementation("androidx.room:room-common:2.6.0")
     implementation("androidx.room:room-ktx:2.6.0")
+
+//    val roomVersion = "2.6.0"
+//    implementation("androidx.room:room-runtime:roomVersion")
+//    ksp("androidx.room:room-compiler:$roomVersion")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
