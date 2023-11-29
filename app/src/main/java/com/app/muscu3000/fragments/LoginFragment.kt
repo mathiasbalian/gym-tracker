00caf669783505b7a069bb85fbac114bf5e15b2b
viviendbk <vivien.deblock@edu.devinci.fr>
2023-11-29 18:29:51 +0100

    UI improvement

--- app/src/main/java/com/app/muscu3000/fragments/LoginFragment.kt
+++ app/src/main/java/com/app/muscu3000/fragments/LoginFragment.kt
@@ -4,8 +4,11 @@ import android.content.Context
 import android.content.SharedPreferences
 import android.os.Bundle
 import android.util.Log
+import android.view.MotionEvent
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.Button
+import android.widget.RelativeLayout
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.app.muscu3000.R
@@ -73,6 +76,15 @@ class LoginFragment : Fragment(R.layout.login_fragment) {
             // Navigate to the RegisterFragment when the button is clicked
             findNavController().navigate(R.id.action_loginFragment_to_registerFragment)
         }
+
+        // Set up a touch listener on the parent layout to hide the keyboard when touched outside the input fields
+        val parentLayout: RelativeLayout = view.findViewById(R.id.loginFragment)
+        parentLayout.setOnTouchListener { _, event ->
+            if (event.action == MotionEvent.ACTION_DOWN) {
+                hideKeyboard()
+            }
+            false
+        }
     }
 
     private fun isLoggedIn(): Boolean {
@@ -109,5 +121,10 @@ class LoginFragment : Fragment(R.layout.login_fragment) {
     private fun showErrorDialog(errorMessage: String) {
         val dialogFragment = DialogFragment.newInstance(errorMessage)
         dialogFragment.show(parentFragmentManager, "ErrorDialogFragment")
+    }
+
+    private fun hideKeyboard() {
+        val imm = requireContext().getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view?.windowToken, 0)
     }
 }
--- app/src/main/java/com/app/muscu3000/fragments/RegisterFragment.kt
+++ app/src/main/java/com/app/muscu3000/fragments/RegisterFragment.kt
@@ -1,8 +1,12 @@
 package com.app.muscu3000.fragments
 
+import android.content.Context
 import android.os.Bundle
+import android.view.MotionEvent
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.Button
+import android.widget.RelativeLayout
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.app.muscu3000.R
@@ -20,5 +24,20 @@ class RegisterFragment : Fragment(R.layout.register_fragment) {
             // Navigate to the RegisterFragment when the button is clicked
             findNavController().navigate(R.id.action_registerFragment_to_loginFragment)
         }
+
+        // Set up a touch listener on the parent layout to hide the keyboard when touched outside the input fields
+        val parentLayout: RelativeLayout = view.findViewById(R.id.registerFragment)
+        parentLayout.setOnTouchListener { _, event ->
+            if (event.action == MotionEvent.ACTION_DOWN) {
+                hideKeyboard()
+            }
+            false
+        }
+    }
+
+
+    private fun hideKeyboard() {
+        val imm = requireContext().getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view?.windowToken, 0)
     }
 }
