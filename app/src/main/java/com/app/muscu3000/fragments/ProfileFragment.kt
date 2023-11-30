0a21225f1fe3c7fb5dd6413e41eac20726cce37d
mathiasbalian <mathias.balian92@gmail.com>
2023-11-30 18:14:28 +0100

    created profile fragment

---
+++ app/src/main/java/com/app/muscu3000/fragments/ProfileFragment.kt
@@ -0,0 +1,30 @@
+package com.app.muscu3000.fragments
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import com.app.muscu3000.R
+import com.app.muscu3000.dao.UserDao
+import com.app.muscu3000.database.AppDatabase
+
+class ProfileFragment : Fragment(R.layout.profile_fragment) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val userName: TextView = view.findViewById(R.id.userFullNameTextView)
+
+
+        val logOutButton: Button = view.findViewById(R.id.logOutButton)
+        logOutButton.setOnClickListener {
+
+        }
+
+        val internetButton: Button = view.findViewById(R.id.internetPageButton)
+        internetButton.setOnClickListener {
+
+        }
+    }
+
+}
