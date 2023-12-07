fe65dba9c413a7b2d018bc418cfa6929e9e13d36
mathiasbalian <mathias.balian92@gmail.com>
2023-12-07 12:52:11 +0100

    added profile fragment

--- app/src/main/java/com/app/muscu3000/fragments/ProfileFragment.kt
+++ app/src/main/java/com/app/muscu3000/fragments/ProfileFragment.kt
@@ -5,6 +5,7 @@ import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.app.muscu3000.R
 import com.app.muscu3000.dao.UserDao
 import com.app.muscu3000.database.AppDatabase
@@ -23,7 +24,7 @@ class ProfileFragment : Fragment(R.layout.profile_fragment) {
 
         val internetButton: Button = view.findViewById(R.id.internetPageButton)
         internetButton.setOnClickListener {
-
+            //findNavController().navigate(R.id.action_profileFragment_to_internetFragment)
         }
     }
 
