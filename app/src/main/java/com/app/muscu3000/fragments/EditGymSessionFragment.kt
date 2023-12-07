4d0b8f696b2656230758e8acd6c359aab1a4d126
viviendbk <vivien.deblock@edu.devinci.fr>
2023-12-07 12:45:16 +0100

    display bug fixed

---
+++ app/src/main/java/com/app/muscu3000/fragments/EditGymSessionFragment.kt
@@ -0,0 +1,8 @@
+package com.app.muscu3000.fragments
+
+import androidx.fragment.app.Fragment
+import com.app.muscu3000.R
+
+class EditGymSessionFragment : Fragment(R.layout.edit_gym_session_fragment) {
+
+}
