ff0ed580d62daa72ad36daa875325ea31c167add
mathiasbalian <mathias.balian92@gmail.com>
2023-12-10 14:07:56 +0100

    implemented navigation bar's logic

--- app/src/main/java/com/app/muscu3000/MainActivity.kt
+++ app/src/main/java/com/app/muscu3000/MainActivity.kt
@@ -4,11 +4,19 @@ import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
 import com.app.muscu3000.database.AppDatabase
+import com.app.muscu3000.databinding.ActivityMainBinding
 import com.app.muscu3000.model.User
+import com.google.android.material.bottomnavigation.BottomNavigationItemView
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+    private lateinit var navBar: BottomNavigationView
 
     companion object {
         lateinit var database: AppDatabase
@@ -18,38 +26,63 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        (supportFragmentManager.findFragmentById(R.id.navHostFragmentContainerView) as NavHostFragment).also {
+            navController = it.navController
+        }
+
+        navBar = findViewById(R.id.bottomNavigationView)
+        navBar.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.homeNavIcon -> {
+                    navController.navigate(R.id.homeFragment)
+                    true
+                }
+                R.id.profileNavIcon -> {
+                    navController.navigate(R.id.profileFragment)
+                    true
+                }
+                R.id.addSessionNavIcon -> {
+                    navController.navigate(R.id.addGymSessionFragment)
+                    true
+                }
+                else -> false
+            }
+        }
+
+
+
         // Initialize the Room database
         database = AppDatabase.getInstance(this)!!
 
-        // Assuming you have an initialized database instance (MainActivity.database)
-        var userDao = database.userDao()
-
-        val user = User(
-            email = "test@example.com",
-            password = "password123",
-            fullName = "Test User"
-        )
-        // Launch a coroutine to insert the user into the database
-        lifecycleScope.launch {
-            userDao.insertUser(user)
-        }
-
-        // Assuming you have an initialized database instance (MainActivity.database)
-        userDao = database.userDao()
-
-        // Launch a coroutine to retrieve the user by their ID
-        lifecycleScope.launch {
-            val retrievedUser = userDao.getUserById(1) // Assuming 1 is the ID of the user you inserted
-
-            if (retrievedUser != null) {
-                // Data retrieval was successful
-                // Log or display the retrieved user's information
-                Log.d("MainActivity", "Retrieved User: $retrievedUser")
-            } else {
-                // User not found or an error occurred
-                Log.e("MainActivity", "Failed to retrieve user")
-            }
-        }
+//        // Assuming you have an initialized database instance (MainActivity.database)
+//        var userDao = database.userDao()
+//
+//        val user = User(
+//            email = "test@example.com",
+//            password = "password123",
+//            fullName = "Test User"
+//        )
+//        // Launch a coroutine to insert the user into the database
+//        lifecycleScope.launch {
+//            userDao.insertUser(user)
+//        }
+//
+//        // Assuming you have an initialized database instance (MainActivity.database)
+//        userDao = database.userDao()
+//
+//        // Launch a coroutine to retrieve the user by their ID
+//        lifecycleScope.launch {
+//            val retrievedUser = userDao.getUserById(1) // Assuming 1 is the ID of the user you inserted
+//
+//            if (retrievedUser != null) {
+//                // Data retrieval was successful
+//                // Log or display the retrieved user's information
+//                Log.d("MainActivity", "Retrieved User: $retrievedUser")
+//            } else {
+//                // User not found or an error occurred
+//                Log.e("MainActivity", "Failed to retrieve user")
+//            }
+//        }
 
 
     }
