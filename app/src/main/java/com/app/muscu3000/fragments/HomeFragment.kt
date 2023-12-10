9c55837678965369ead6d9995c8e1664a9f53d85
mathiasbalian <mathias.balian92@gmail.com>
2023-12-10 16:39:34 +0100

    changed logic to observe viewmodel's sessionList for recyclerview

--- app/src/main/java/com/app/muscu3000/fragments/HomeFragment.kt
+++ app/src/main/java/com/app/muscu3000/fragments/HomeFragment.kt
@@ -3,6 +3,9 @@ package com.app.muscu3000.fragments
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -11,30 +14,24 @@ import com.app.muscu3000.Adapter.HomeGymSessionAdapter
 import com.app.muscu3000.MainActivity
 import com.app.muscu3000.R
 import com.app.muscu3000.model.GymSession
+import com.app.muscu3000.viewmodels.GymSessionsViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
 class HomeFragment : Fragment(R.layout.home_fragment) {
 
-    private lateinit var sessionList: List<GymSession>
+    private val gymSessionViewModel: GymSessionsViewModel by activityViewModels()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         val gymSessionRecyclerView: RecyclerView = view.findViewById(R.id.gymSessionRecyclerView)
+        gymSessionRecyclerView.layoutManager = LinearLayoutManager(requireContext())
 
-        lifecycleScope.launch {
-            withContext(Dispatchers.IO){
-                sessionList = MainActivity.database.gymSessionDao().getAllGymSessions()
-            }
+        gymSessionViewModel.sessionList.observe(viewLifecycleOwner, Observer {
+            gymSessionRecyclerView.adapter = HomeGymSessionAdapter(it, gymSessionViewModel)
+        })
 
-            withContext(Dispatchers.Main) {
-                println(sessionList)
-                val adapter = HomeGymSessionAdapter(sessionList)
-                gymSessionRecyclerView.adapter = adapter
-                gymSessionRecyclerView.layoutManager = LinearLayoutManager(requireContext())
-            }
-        }
     }
 }
