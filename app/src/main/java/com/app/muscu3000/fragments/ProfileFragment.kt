049847c223ed34d27d5a3655e6dbcd098635ca13
mathiasbalian <mathias.balian92@gmail.com>
2023-12-11 20:21:44 +0100

    added sharedpreferences logic to log out button

--- app/src/main/java/com/app/muscu3000/fragments/ProfileFragment.kt
+++ app/src/main/java/com/app/muscu3000/fragments/ProfileFragment.kt
@@ -1,5 +1,7 @@
 package com.app.muscu3000.fragments
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
@@ -12,15 +14,19 @@ import com.app.muscu3000.dao.UserDao
 import com.app.muscu3000.database.AppDatabase
 
 class ProfileFragment : Fragment(R.layout.profile_fragment) {
+
+    private lateinit var sharedPreferences: SharedPreferences
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         val userName: TextView = view.findViewById(R.id.userFullNameTextView)
 
-
+        sharedPreferences = requireActivity().getPreferences(Context.MODE_PRIVATE)
         val logOutButton: Button = view.findViewById(R.id.logOutButton)
         logOutButton.setOnClickListener {
-
+            sharedPreferences.edit().putBoolean("is_logged_in", false).apply()
+            findNavController().navigate(R.id.loginFragment)
         }
 
         val internetButton: ImageButton = view.findViewById(R.id.internetPageButton)
