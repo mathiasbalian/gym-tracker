5e572fba1cdb648b64770c40cf2d54a44b58ca64
mathiasbalian <mathias.balian92@gmail.com>
2023-12-14 02:20:04 +0100

    removed unnecessary attributes and added createTodo method

--- app/src/main/java/com/app/muscu3000/viewmodels/TodoViewModel.kt
+++ app/src/main/java/com/app/muscu3000/viewmodels/TodoViewModel.kt
@@ -18,12 +18,10 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 class TodoViewModel : ViewModel() {
     private var _todos: MutableLiveData<MutableList<Todo>> = MutableLiveData()
-    private var _createdTodos: MutableLiveData<MutableList<Todo>> = MutableLiveData()
     private var _internetError: MutableLiveData<Boolean> = MutableLiveData(false)
 
     var internetError: LiveData<Boolean> = _internetError
-    var todos: LiveData<MutableList<Todo>> = _todos
-    var createdTodos: LiveData<MutableList<Todo>> = _createdTodos
+    var todos: MutableLiveData<MutableList<Todo>> = _todos
 
     private val retrofit: Retrofit = Retrofit.Builder()
         .baseUrl("https://dummyjson.com/")
@@ -32,24 +30,56 @@ class TodoViewModel : ViewModel() {
 
     private val apiService: ApiService = retrofit.create(ApiService::class.java)
 
-
     init {
         getTodos()
     }
 
-    private fun getTodos(){
+    fun getTodos(){
         val todoCall = apiService.getTodos()
         todoCall.enqueue(object: Callback<TodoListResponse> {
             override fun onResponse(call: Call<TodoListResponse>, response: Response<TodoListResponse>){
                 if(response.isSuccessful) {
                     _todos.value = response.body()?.todos as MutableList<Todo>
                     todos = _todos
+                    _internetError.value = false
+                    internetError = _internetError
+                }
+                else {
+                    _internetError.value = true
+                    internetError = _internetError
                 }
             }
 
             override fun onFailure(call: Call<TodoListResponse>, t: Throwable) {
                 println(t)
+                _internetError.value = true
+                internetError = _internetError
             }
+        })
+    }
+
+    fun createTodo(todoContent: String, todoIsCompleted: Boolean){
+        val todoCall = apiService.postTodo(Todo(id = 1, todo = todoContent, completed = todoIsCompleted, userId = 1))
+        todoCall.enqueue(object: Callback<Todo> {
+            override fun onResponse(call: Call<Todo>, response: Response<Todo>) {
+                if(response.isSuccessful){
+                    _todos.value?.add(response.body()!!)
+                    todos.value = _todos.value
+                    _internetError.value = false
+                    internetError = _internetError
+                }
+                else {
+                    _internetError.value = true
+                    internetError = _internetError
+                }
+            }
+
+            override fun onFailure(call: Call<Todo>, t: Throwable) {
+                println(t)
+                _internetError.value = true
+                internetError = _internetError
+            }
+
         })
     }
 
