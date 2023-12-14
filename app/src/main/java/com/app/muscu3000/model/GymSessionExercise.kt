209dc5191a8c9ab374510d997bb309a6438a7c21
mathiasbalian <mathias.balian92@gmail.com>
2023-12-14 03:37:58 +0100

    removed foreign keys that were causing an issue on first session creation

--- app/src/main/java/com/app/muscu3000/model/GymSessionExercise.kt
+++ app/src/main/java/com/app/muscu3000/model/GymSessionExercise.kt
@@ -6,20 +6,20 @@ import androidx.room.ForeignKey
 @Entity(
     tableName = "GymSessionExercise",
     primaryKeys = ["gymSessionId", "exerciseId"],
-    foreignKeys = [
-        ForeignKey(
-            entity = GymSession::class,
-            parentColumns = ["gymSessionId"],
-            childColumns = ["gymSessionId"],
-            onDelete = ForeignKey.CASCADE
-        ),
-        ForeignKey(
-            entity = Exercise::class,
-            parentColumns = ["exerciseId"],
-            childColumns = ["exerciseId"],
-            onDelete = ForeignKey.CASCADE
-        )
-    ]
+//    foreignKeys = [
+//        ForeignKey(
+//            entity = GymSession::class,
+//            parentColumns = ["gymSessionId"],
+//            childColumns = ["gymSessionId"],
+//            onDelete = ForeignKey.CASCADE
+//        ),
+//        ForeignKey(
+//            entity = Exercise::class,
+//            parentColumns = ["exerciseId"],
+//            childColumns = ["exerciseId"],
+//            onDelete = ForeignKey.CASCADE
+//        )
+//    ]
 )data class GymSessionExercise(
     val gymSessionId: Long,
     val exerciseId: Long
